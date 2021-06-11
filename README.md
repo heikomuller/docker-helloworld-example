a20da2896744aab126c757835119809571c2dbfb
Heiko Mueller <heiko.muller@gmail.com>
2021-06-11 07:31:38 -0400

    Update schemes for file references

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Hello World - Docker Demo
 =========================
 
-This repository contains the code and configuration file for a **Hello World** example that defines three workflow operators for registration with an experimental workflow engine that uses Docker to execute serial workflows.
+This repository contains the code and configuration file for a **Hello World** example that defines three workflow operators for registration with an experimental version of the [drama workflow engine](https://github.com/heikomuller/drama/tree/feature/docker) that uses Docker to execute serial workflows.
+
+Use the example script [install_and_run_from_github.py](https://github.com/heikomuller/drama/blob/feature/docker/examples/docker/install_and_run_from_github.py) to run a workflow containing all operators that are defined in this repository.
--- code/download.py
+++ code/download.py
@@ -6,26 +6,19 @@ from io import StringIO
 from pathlib import Path
 
 
-URLS = [
-    'https://www.gutenberg.org/files/3201/files/NAMES-F.TXT',
-    'https://www.gutenberg.org/files/3201/files/NAMES-M.TXT'
-]
+URL = 'https://www.gutenberg.org/files/3201/files/NAMES.TXT'
 
 
 def main(outputfile):
     """
-    Download files with popular male and female names and write the combined
-    result to the given output file.
+    Download files with popular male and female names to the given output file.
     """
-    names = list()
-    for url in URLS:
-        r = requests.get(url)
-        r.raise_for_status()
-        with StringIO(r.text, newline='') as f:
-            names.extend([line.strip() for line in f])
-    with outputfile.open('wt') as f:
-        for name in names:
-            f.write(f"{name}\n")
+    r = requests.get(URL)
+    r.raise_for_status()
+    with StringIO(r.text, newline='') as f:
+        with outputfile.open('wt') as f:
+            for name in [line.strip() for line in f]:
+                f.write(f"{name}\n")
 
 
 if __name__ == '__main__':
