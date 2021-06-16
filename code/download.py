5435a6cc91093e0720d536d13737e57d484bb683
Heiko Mueller <heiko.muller@gmail.com>
2021-06-16 12:20:28 -0400

    Add more download operators

--- code/download.py
+++ code/download.py
@@ -5,14 +5,22 @@ import sys
 from pathlib import Path
 
 
-URL = 'https://www.gutenberg.org/files/3201/files/NAMES.TXT'
+URL_ALL = 'https://www.gutenberg.org/files/3201/files/NAMES.TXT'
+URL_FEMALE = 'https://www.gutenberg.org/files/3201/files/NAMES-F.TXT'
+URL_MALE = 'https://www.gutenberg.org/files/3201/files/NAMES-M.TXT'
 
 
-def main(outputfile):
+def main(outputfile, gender=None):
     """
     Download files with popular male and female names to the given output file.
     """
-    r = requests.get(URL)
+    if gender == 'female':
+        url = URL_FEMALE
+    elif gender == 'male':
+        url = URL_MALE
+    else:
+        url = URL_ALL
+    r = requests.get(url)
     r.raise_for_status()
     with outputfile.open('wt') as f:
         f.write(r.text)
@@ -22,8 +30,9 @@ if __name__ == '__main__':
     args = sys.argv[1:]
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--gender", required=False)
     parser.add_argument("-o", "--outputfile", required=True)
 
     parsed_args = parser.parse_args(args)
 
-    main(outputfile=Path(parsed_args.outputfile))
+    main(outputfile=Path(parsed_args.outputfile), gender=parsed_args.gender)
